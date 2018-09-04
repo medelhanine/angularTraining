c6fe32fbb59090f73fa17ecb30da58c735bd4193
med <mohamedelhanine94@gmail.com>
2018-09-04 16:28:49 +0100

    testing

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,19 +5,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses', // <courses></courses>
     template: `
-    <h2>{{ title }}</h2>
-    <ul>
-    <li *ngFor="let course of courses">
-    {{ course }}
-    </li>
-    </ul>
-    ` // string interpolation
+    <button (click)="onSave()" class="btn btn-primary">Save</button>`
 })
 export class CoursesComponent {
-    title = "List of courses";
-    courses;
-
-    constructor(service: CoursesService) { // decouple the service from the component
-        this.courses = service.getCourses();
+    onSave() {
+        console.log("button was clicked");
     }
 }
